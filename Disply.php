418e6a12d0ff60eb6c60f0acdac761419eba7953
Robiulalways <shobujkhan7212@gmail.com>
2024-10-03 11:18:49 +0600

    Add files via upload

---
+++ Disply.php
@@ -0,0 +1,48 @@
+<?php
+
+
+/*
+
+functions.php // connection korta hoba ai filta
+include_once('opction/cs-framework.php');
+
+
+*/
+
+
+/*
+
+disply show text 
+<?php echo cs_get_option('fields_2');  ?>
+
+
+disply show img 
+<img  src="<?php echo esc_url(wp_get_attachment_image_src(cs_get_option('fields_1'),'full')[0]); ?>" alt="Image">
+
+
+
+
+
+
+disply show fields_group
+ 
+		<?php 
+		
+			$icons = cs_get_option('fields_group_1');
+			if(is_array($icons)){
+			foreach( $icons as $icons_value ){
+				
+				
+		?>
+							
+							
+		 <a class="" href="#"><i class="<?php echo $icons_value['fields_4']?>"></i></a>
+							
+		<?php 
+		
+			}
+			}
+
+		?>
+
+*/
